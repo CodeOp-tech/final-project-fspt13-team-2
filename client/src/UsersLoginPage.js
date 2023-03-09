0e2cfd3f57809c1e0eaf9473fbfe876661c3b3c9
Catalina Singer <catalina.singer@gmail.com>
2023-03-09 13:24:44 +0100

    endpoints added to users route for users register and login features

--- client/src/UsersLoginPage.js
+++ client/src/UsersLoginPage.js
@@ -1,4 +1,38 @@
+import { useState } from "react";
+import axios from 'axios';
+
 export default function UsersLoginPage() {
+  const [credentials, setCredentials] = useState({
+    username: "test",
+    password: "test"
+  })
+
+  const {username, password} = credentials;
+
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+    setCredentials({...credentials, [name]: value });
+  };
+
+  const login = async () => {
+    try {
+      const { data } = await axios("users/login", {
+        method: "POST",
+        data: credentials
+      });
+    
+      //store it locally
+      localStorage.setItem("token", data.token);
+      console.log(data.message, data.token);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const logout = () => {
+    localStorage.removeItem("token");
+  }
+
     return (
       <div>
         <h1>Log in</h1>
