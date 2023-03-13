53c4fe2154a62255d77b1d0fe064af8eee39aed5
Catalina Singer <catalina.singer@gmail.com>
2023-03-13 11:54:09 +0100

    updated backend same as Isabel´s

--- model/init.sql
+++ model/init.sql
@@ -2,36 +2,29 @@ DROP TABLE if exists posts;
 DROP TABLE if exists users;
 DROP TABLE if exists votes;
 
-
 CREATE TABLE posts (
   id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   user_id INT NOT NULL,
-  content TEXT NOT NULL,
-  created_date DATETIME NOT NULL,
-  image TEXT NOT NULL
+  content TEXT,
+  created_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  image VARCHAR(500)
 );
 
-INSERT INTO posts (user_id, content, created_date, image) VALUES(1, "Banana bread", 20230305 08:18:45, "https://www.simplyrecipes.com/thmb/tR-5eHAZ3lgNR6Yvu3yxdHMNpk8=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Simply-Recipes-Easy-Banana-Bread-LEAD-2-2-63dd39af009945d58f5bf4c2ae8d6070.jpg")
 
 
 CREATE TABLE users (
   id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  username VARCHAR(25) NOT NULL,
+  nick VARCHAR(25) NOT NULL,
   email VARCHAR(89) NOT NULL,
-  password VARCHAR(50) NOT NULL
+  password TEXT NOT NULL
 );
 
-INSERT INTO users (username, email, password) VALUES("john89", "johndoe@gmail.com", "secretpassword");
+INSERT INTO users (nick, email, password) VALUES('john89', "johndoe@gmail.com", "secretpassword");
 
 
 CREATE TABLE votes (
   id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   post_id INT NOT NULL,
-  wow INT NOT NULL,
-  meh INT NOT NULL
+  user_id INT NOT NULL
+  wow TINYINT NOT NULL,
 );
-
-INSERT INTO votes (post_id, wow, meh) VALUES(1, 3, 2);
-
-
-
--- routes/posts.js
+++ routes/posts.js
@@ -11,19 +11,42 @@ router.get('/', function(req, res, next) {
     .catch(err => res.status(500).send(err));
 });
 
-module.exports = router;
+/* CREATE new post. */
+router.post("/create", async (req, res) => {
+  const {user_id, content=null, image=null} = req.body;
 
-/* POST posts */
+  if (!user_id){
+    res.status(400).send({ message: "user_id is missing"});
+    return;
+  }
 
-router.post("/posts", function (req, res) {
-  const body = req.body
-  const content = req.body.content
-  const image = req.body.image
+  if (!content && !image){
+    res.status(400).send({ message: "content or image is missing"});
+    return;
+  }
+
+
 
   try {
-    await db(`INSERT INTO posts (content, image) values ('${content}', ${image})`)
-    res.send(201)
+    const txtcontent = content ? `'${content}'`: null; 
+    const txtimage = image ? `'${image}'`: null;
+
+    await db(`INSERT INTO posts (user_id, content, image) VALUES (${user_id}, ${txtcontent}, ${txtimage})`);
+    res.send({ message: "New post created"});
   } catch(error) {
     res.status(500).send(error)
   }
 });
+
+/* COUNT number of wow for each post. */
+router.get('/wow', function(req, res, next) {
+  db("SELECT posts.id, COUNT(votes.wow) AS wow_count FROM posts LEFT JOIN votes ON posts.id = votes.post_id WHERE votes.wow = true GROUP BY posts.id;")
+  // db("SELECT posts.id, COUNT(CASE WHEN votes.wow = true THEN 1 END) AS wow_count, COUNT(CASE WHEN votes.wow = false 1 END) AS meh_count FROM posts LEFT JOIN votes ON posts.id = votes.post_id GROUP BY posts.id;")
+    .then(results => {
+      res.send(results.data);
+    })
+    .catch(err => res.status(500).send(err));
+});
+
+
+module.exports = router;
